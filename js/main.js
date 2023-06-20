2d2fd8ea259908a98f40173f5552bb420b27898b
Pasztor Adam <adam.psztr@icloud.com>
2023-06-20 23:07:22 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 let pics = document.querySelectorAll(".polaroid");
 let counter = 0;
+let pre = 3;
+
 document.querySelector("#js-icon3").addEventListener("click", () => {
-	pics[counter].classList.toggle("bigImg");
-	counter = counter < 3 ? counter++ : 0;
+	if (counter < 4) {
+		pics[counter].classList.add("bigImg");
+		pics[pre].classList.remove("bigImg");
+		counter++;
+		pre = counter - 1;
+	} else {
+		pics[pre].classList.remove("bigImg");
+		counter = 0;
+		pre = 3;
+	}
+});
+
+document.querySelector("#js-icon3").addEventListener("touchstart", () => {
+	if (counter < 4) {
+		pics[counter].classList.add("bigImg");
+		pics[pre].classList.remove("bigImg");
+		counter++;
+		pre = counter - 1;
+	} else {
+		pics[pre].classList.remove("bigImg");
+		counter = 0;
+		pre = 3;
+	}
+	console.log(counter)
+	console.log(pre)
 });
